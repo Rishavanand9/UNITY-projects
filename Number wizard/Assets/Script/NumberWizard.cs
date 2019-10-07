ba66fb7738d201beccd1613d36f8822a0d9ed6a4
Rishavanand9 <rishavanand1997@gmail.com>
2019-10-07 12:34:49 +0530

    Number Wizard

---
+++ Number wizard/Assets/Script/NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+        max = max + 1;
+    }
+
+    public void onPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void onPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    public void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+
+    }
+
+}
